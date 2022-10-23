4ce7be0a5eda7cf8668e4e9d9ee0e8e0fd48ab96
SonjaMae <59752342+SonjaMae@users.noreply.github.com>
2022-10-23 00:48:35 -0600

    adding redirect to functions.php

--- wp-content/themes/bosa-child/functions.php
+++ wp-content/themes/bosa-child/functions.php
@@ -6,3 +6,18 @@ wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/style.css', arr
 //wp_enqueue_script('main', get_stylesheet_directory_uri().'/includes/js/main.js', array('jquery'), '3.1.1', true);
 }
 add_action('wp_enqueue_scripts', 'bosa_child_styles');
+
+// Redirect function for the SOAER Presentations page.
+add_action('template_redirect', function() {
+    // ID of the redirect page
+    if (!is_page(1730)) {
+        return;
+    }
+    // URL of page with form that needs to be filled before access:
+    if (wp_get_referer() === 'https://sonjamae.ca/mrbb/soaer-committee/soaer-presentations/') {
+        return;
+    }
+	// Redirect to the page with the form:
+    wp_redirect('https://sonjamae.ca/mrbb/soaer-committee/soaer-presentations/');
+    exit;
+} );
