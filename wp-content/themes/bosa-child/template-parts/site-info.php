1c2b7aecd2a90801170cfc9116784f27c709bf60
SonjaMae <59752342+SonjaMae@users.noreply.github.com>
2022-11-07 16:02:15 -0700

    site-info.php controls bottom-footer

--- wp-content/themes/bosa-child/template-parts/site-info.php
+++ wp-content/themes/bosa-child/template-parts/site-info.php
@@ -1,20 +1,30 @@
-<?php
-/**
- * Template part for displaying site info
- *
- * @package Bosa
- */
-
-?>
-
-<div class="site-info">
-	<?php echo wp_kses_post( html_entity_decode( esc_html__( 'Copyright &copy; ' , 'bosa' ) ) );
-		echo esc_html( date('Y') );
-		printf( esc_html__( ' Bosa. Powered by', 'bosa' ) );
-	?>
-	<a href="<?php echo esc_url( __( '//bosathemes.com', 'bosa' ) ); ?>" target="_blank">
-		<?php
-			printf( esc_html__( 'Bosa Themes', 'bosa' ) );
-		?>
-	</a>
+<?php
+
+/**
+
+ * Template part for displaying site info
+
+ *
+
+ * @package Bosa
+
+ */
+
+
+
+?>
+
+
+
+<div class="site-info">
+
+	<?php echo wp_kses_post( html_entity_decode( esc_html__( 'Copyright &copy; ' , 'Mackenzie River Basin Board' ) ) );
+		echo esc_html( date('Y') );
+		printf( esc_html__( ' Mackenzie River Basin Board', 'bosa' ) );
+	?>
+
+	<a href="<?php echo esc_url( __( '//bosathemes.com', 'bosa' ) ); ?>" target="_blank">
+		<?php printf( esc_html__( 'Bosa Themes', 'bosa' ) ); ?>
+	</a>
+
 </div><!-- .site-info -->
