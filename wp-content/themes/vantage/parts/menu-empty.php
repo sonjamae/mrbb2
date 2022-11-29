3dfee16b7a3bad8df833ec367ce27ada64d56683
SonjaMae <59752342+SonjaMae@users.noreply.github.com>
2022-11-28 22:22:58 -0700

    removing vantage theme, and another css checkpoint

--- wp-content/themes/vantage/parts/menu-empty.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Part Name: Empty Menu
- */
-?>
-<nav class="site-navigation main-navigation primary">
-	<div class="full-container">
-	</div>
-</nav><!-- .site-navigation .main-navigation -->
